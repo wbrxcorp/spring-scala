92f9ba3ba26ed1e55bccc5c7c1e56e0aa7c397b2
Tomoatsu Shimada <shimada@walbrix.com>
2015-02-26 11:40:56 +0900

    scala repl support

---
+++ src/main/scala/com/walbrix/spring/REPL.scala
@@ -0,0 +1,26 @@
+package com.walbrix.spring
+
+import org.springframework.context.ApplicationContext
+import org.springframework.context.support.{ClassPathXmlApplicationContext, FileSystemXmlApplicationContext}
+
+/**
+ * Created by shimarin on 15/02/26.
+ */
+class _DB extends ScalikeJdbcSupport {
+
+}
+
+object REPL {
+  private var applicationContext:ApplicationContext = _
+
+  def apply(name:String):AnyRef = applicationContext.getBean(name)
+
+  def DB:_DB = apply("DB").asInstanceOf[_DB]
+
+  def main(args:Array[String]):Unit = {
+    applicationContext = new ClassPathXmlApplicationContext("/com/walbrix/spring/REPL.xml")
+    System.setProperty("scala.usejavacp","true")
+    val runner = Class.forName("scala.tools.nsc.MainGenericRunner")
+    runner.getMethod("main", classOf[Array[String]]).invoke(null, args)
+  }
+}
