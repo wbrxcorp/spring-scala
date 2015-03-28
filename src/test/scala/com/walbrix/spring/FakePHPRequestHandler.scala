bb1b54f3737704499aa289aa521d2ef08611dea0
Tomoatsu Shimada <shimada@walbrix.com>
2015-03-28 12:07:12 +0900

    Fake PHP example

---
+++ src/test/scala/com/walbrix/spring/FakePHPRequestHandler.scala
@@ -0,0 +1,17 @@
+package com.walbrix.spring
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.{ResponseBody, RequestMethod, RequestMapping}
+
+/**
+ * Created by shimarin on 15/03/28.
+ */
+@Controller
+@RequestMapping(Array(""))
+class FakePHPRequestHandler {
+  @RequestMapping(value=Array("hello"), method=Array(RequestMethod.GET))
+  @ResponseBody
+  def hello():Tuple1[String] = {
+    Tuple1("Hello, World!")
+  }
+}
