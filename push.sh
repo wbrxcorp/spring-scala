c6166ecb3337acf84eab00a26949c7710ccb8f93
Tomoatsu Shimada <shimada@walbrix.com>
2015-04-02 12:25:13 +0900

    scalikejdbc refresh push script improved

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-version=$1
+DIRNAME=`dirname $0`
+version=`egrep '^version\s*=' $DIRNAME/build.gradle|egrep -o '[0-9\.]+'`
 sed 's/__VERSION__/'$version'/' spring-scala.pom > build/libs/spring-scala-$version.pom
 s3cmd put -P build/libs/spring-scala-$version*.* s3://mvn.walbrix.com/com/walbrix/spring-scala/$version/
 
