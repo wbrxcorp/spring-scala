5e36c95aef1141f99ce4925ac55855857d3ae738
Tomoatsu Shimada <shimada@walbrix.com>
2015-04-17 08:27:11 +0900

    E2ETest

---
+++ src/test/scala/E2ETest.md
@@ -0,0 +1,10 @@
+Jettyでサービスを起動し Seleniumで E2Eテストを走らせるサンプル
+
+### このソースからわかること
+
+- ScalikeJDBCのSQLログ表示を設定する方法
+- Jettyの起動方法
+- OSの判定方法
+- ChromeDriverの実行バイナリを指定する方法
+- ScalaTest で Seleniumをドライブする方法
+
---
+++ src/test/scala/E2ETest.scala
@@ -0,0 +1,64 @@
+import java.net.URLClassLoader
+
+import org.eclipse.jetty.server.{ServerConnector, Server}
+import org.eclipse.jetty.webapp.WebAppContext
+import org.junit._
+import org.scalatest.selenium.Chrome
+import scalikejdbc.{LoggingSQLAndTimeSettings, GlobalSettings}
+
+/**
+ * Created by shimarin on 15/04/17.
+ */
+class E2ETest extends Chrome {
+  @Before def tearUp:Unit = {
+    go to "http://localhost:%d/".format(E2ETest.getPort())
+  }
+
+  @After def tearDown:Unit = {
+    close
+  }
+
+  @Test def foo():Unit = {
+
+  }
+}
+
+object E2ETest {
+  private var server:Server = _
+
+  // ScalikeJDBC log settings
+  GlobalSettings.loggingSQLAndTime = new LoggingSQLAndTimeSettings(
+    enabled = true,
+    singleLineMode = true,
+    logLevel = 'DEBUG
+  )
+
+  val driverPath = System.getProperty("os.name") match {
+    case x if x.startsWith("Windows") => Some("src/test/chromedriver.exe")
+    case x if x.startsWith("Mac") => Some("src/test/chromedriver")
+    // if Linux, /usr/bin/chromedriver is expected
+    case _ => None
+  }
+  driverPath.foreach(System.setProperty("webdriver.chrome.driver", _))
+
+  def join():Unit = server.join()
+  def getPort():Int = server.getConnectors()(0).asInstanceOf[ServerConnector].getLocalPort()
+
+  @BeforeClass def before():Unit = {
+    server = new Server(0)
+    val webapp = new WebAppContext()
+
+    webapp.setResourceBase("src/examples/webapp")
+    webapp.setContextPath("/")
+    webapp.setClassLoader(new URLClassLoader(new Array[java.net.URL](0), this.getClass().getClassLoader()))
+    webapp.setInitParameter("org.eclipse.jetty.servlet.Default.useFileMappedBuffer", "false")
+    server.setHandler(webapp)
+    server.start()
+  }
+
+  @AfterClass def after():Unit = {
+    server.stop()
+    server.destroy()
+  }
+
+}
