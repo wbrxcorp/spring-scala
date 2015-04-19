e6143343a9ea642d9d43179c0cf034faeb24d539
Tomoatsu Shimada <shimada@walbrix.com>
2015-04-19 11:38:22 +0900

    Apply loan pattern

--- src/examples/scala/db/migration/V9999__TestData.md
+++ src/examples/scala/db/migration/V9999__TestData.md
@@ -16,9 +16,28 @@ H2などのインメモリデータベースで開発をしている場合は、
     - カナが全角に統一される
     - 英数字が半角に統一される
     - 見た目が同一でコードの異なる文字が統一される (※正確さを犠牲にしてわかりやすさを取った説明)
+- [Loanパターン](http://www.ne.jp/asahi/hishidama/home/tech/scala/sample/using.html)を使ってリソースのクローズを確実に行う方法
 
 ### 関連
 
 - [V0001__Initial_version.sql](${contextRoot}/src/examples/resources/db/migration/V0001__Initial_version.sql) - スキーマ
 - [com.walbrix.flyway.ScalikeJdbcMigration](${contextRoot}/src/main/scala/com/walbrix/flyway/ScalikeJdbcMigration.scala) - Flywayの Migrationを ScalikeJDBCで書くための奴
 - [api-servlet.xml](${contextRoot}/src/examples/webapp/WEB-INF/api-servlet.xml) - Spring MVCの DispatcherServletがロードされた時に FlywayのMigrationを実行する例
+
+### Loanパターン
+
+<blockquote class="twitter-tweet" lang="en"><p><a href="https://twitter.com/shimariso">@shimariso</a> 興味深いですね。本筋ではないですが close 処理は using(res) { r =&gt; } みたいに loan pattern にする方がサンプルとしては適切かもしれません。 <a href="https://t.co/XvZ81K98gT">https://t.co/XvZ81K98gT</a></p>&mdash; seratch_ja (@seratch_ja) <a href="https://twitter.com/seratch_ja/status/589609605925240832">April 19, 2015</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+というご意見を頂いたので、Loanパターンというものを適用してみた。
+
+[Scala using(Hishidama's Scala loan-pattern Memo)](http://www.ne.jp/asahi/hishidama/home/tech/scala/sample/using.html)
+
+によると、「僕が考えたさい強のローンパターン」が色々あるので、これはあくまでパターンであって具体的なやり方まで決まってるわけじゃないみたいだ。
+（確か ScalikeJDBCの中にも独自に usingの定義があった気がする）
+
+結局、
+
+[自動的に閉じるSource - ( ꒪⌓꒪) ゆるよろ日記](http://yuroyoro.hatenablog.com/entry/20101215/1292401822)
+
+これが一番簡単そうだったのでそのまま使わせて頂いた。強い型付であるにもかかわらず Rubyみたいにこういう柔軟なことも出来てしまうのは Scalaのすごいところだと思う。
--- src/examples/scala/db/migration/V9999__TestData.scala
+++ src/examples/scala/db/migration/V9999__TestData.scala
@@ -10,33 +10,33 @@ class V9999__TestData extends com.walbrix.flyway.ScalikeJdbcMigration with com.t
   // NFKC正規化
   private def normalize(str:String):String = Normalizer.normalize(str, Normalizer.Form.NFKC)
 
+  // Loanパターン
+  type Closable = { def close():Unit }
+  def using[A <: Closable,B]( resource:A )( f:A => B ) = try(f(resource)) finally(resource.close)
+
   // マイグレーション実行
   override def migrate(implicit session: scalikejdbc.DBSession): Unit = {
-    Option(this.getClass.getResourceAsStream("/13TOKYO.CSV.xz")).foreach { kenAll =>
+    using(Option(this.getClass.getResourceAsStream("/13TOKYO.CSV.xz")).getOrElse(throw new java.io.FileNotFoundException)) { kenAll =>
       logger.debug("begin loading testdata")
-      try {
-        val xz = new org.tukaani.xz.XZInputStream(kenAll)
-        val isr = new java.io.InputStreamReader(xz, "MS932")
-        val reader = new au.com.bytecode.opencsv.CSVReader(isr, ',', '"')
-        var line = reader.readNext()
-        while (line != null) {
-          val (jis_code, zip_code, city_kana, street_kana, pref, city, street) =
-            (line(0), line(2),
-              normalize(line(4)),
-              normalize(line(5)),
-              line(6), line(7),
-              normalize(line(8)))
-          sql"""insert into zip_code(jis_code,zip_code,city_kana,street_kana,pref,city,street)
-            values(${jis_code},${zip_code},${city_kana},${street_kana},${pref},${city},${street})""".update().apply()
-          line = reader.readNext()
+      using (new org.tukaani.xz.XZInputStream(kenAll)) { xz =>
+        using (new java.io.InputStreamReader(xz, "MS932")) { isr =>
+          using (new au.com.bytecode.opencsv.CSVReader(isr, ',', '"')) { reader =>
+            var line = reader.readNext()
+            while (line != null) {
+              val (jis_code, zip_code, city_kana, street_kana, pref, city, street) =
+                (line(0), line(2),
+                  normalize(line(4)),
+                  normalize(line(5)),
+                  line(6), line(7),
+                  normalize(line(8)))
+              sql"""insert into zip_code(jis_code,zip_code,city_kana,street_kana,pref,city,street)
+              values(${jis_code},${zip_code},${city_kana},${street_kana},${pref},${city},${street})""".update().apply()
+              line = reader.readNext()
+            }
+          }
         }
-        logger.debug("end loading testdata")
-        isr.close()
-        xz.close()
       }
-      finally {
-        kenAll.close()
-      }
+      logger.debug("end loading testdata")
     }
   }
 }
