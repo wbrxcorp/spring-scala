0ee489da682dce50e1878b35d931217ca02b9706
Tomoatsu Shimada <shimada@walbrix.com>
2015-05-19 14:10:58 +0900

    PEM loader

--- src/examples/scala/com/walbrix/spring/PEMRequestHandler.scala
+++ src/examples/scala/com/walbrix/spring/PEMRequestHandler.scala
@@ -1,38 +1,37 @@
 package com.walbrix.spring
 
-import java.io.StringReader
-import java.security.interfaces.RSAKey
-import java.security.{KeyFactory, Security}
-import java.security.spec.PKCS8EncodedKeySpec
+import java.util.Date
+import javax.servlet.http.HttpServletResponse
 
-import org.bouncycastle.asn1.pkcs.CertificationRequest
-import org.bouncycastle.openssl.PEMReader
-import org.springframework.web.bind.annotation.{RequestParam, RestController, RequestMapping}
+import com.fasterxml.jackson.annotation.JsonInclude
+import org.springframework.web.bind.annotation._
 
-case class PemInfo(`type`:String, subject:Option[String] = None)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+case class PemInfo(`type`:String, subject:Option[String] = None, notAfter:Option[Date] = None)
 
 /**
  * Created by shimarin on 15/05/06.
  */
 @RestController
 @RequestMapping(Array("pem"))
-class PEMRequestHandler {
-  @RequestMapping(Array(""))
+class PEMRequestHandler extends com.typesafe.scalalogging.slf4j.LazyLogging {
+  @ExceptionHandler(Array(classOf[IllegalArgumentException]))
+  def badrequest(ex:Exception, response:HttpServletResponse):Unit = {
+    response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage)
+  }
+
+  @RequestMapping(value=Array(""), method=Array(RequestMethod.POST))
   def dosomething(@RequestParam pem:String):PemInfo = {
-    val sr = new StringReader(pem)
-    new PEMReader(sr).readObject() match {
-      case x:CertificationRequest =>
+    val sr = new java.io.StringReader(pem)
+    new org.bouncycastle.openssl.PEMReader(sr).readObject() match {
+      case x:org.bouncycastle.asn1.pkcs.CertificationRequest =>
         PemInfo("csr", Some(x.getCertificationRequestInfo.getSubject.toString))
       case x:java.security.KeyPair =>
         PemInfo("key")
       case x:java.security.cert.X509Certificate =>
-        PemInfo("cert", Some(x.getSubjectDN.toString))
+        PemInfo("cert", Some(x.getSubjectDN.toString), Some(x.getNotAfter))
       case _ =>
-        throw new IllegalArgumentException("Unknown")
+        throw new IllegalArgumentException("Not a valid PEM or unknonwn type of content")
     }
   }
-}
-
-object PEMRequestHandler {
-  Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider)
 }
