5145de5935ced63bd76b5eac66232401452c40aa
Tomoatsu Shimada <shimada@walbrix.com>
2015-09-06 10:53:43 +0900

    Spring 4.2.1

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DIRNAME=`dirname $0`
 version=`egrep '^version\s*=' $DIRNAME/build.gradle|egrep -o '[0-9\.]+'`
-sed 's/__VERSION__/'$version'/' spring-scala.pom > build/libs/spring-scala-$version.pom
+spring_version=`egrep 'springVersion\s*=' build.gradle|egrep -o '[0-9\.]+RELEASE'`
+sed 's/__VERSION__/'$version'/' spring-scala.pom | sed 's/__SPRING_VERSION__/'$spring_version'/' > build/libs/spring-scala-$version.pom
 s3cmd put -P build/libs/spring-scala-$version*.* s3://mvn.walbrix.com/com/walbrix/spring-scala/$version/
-
